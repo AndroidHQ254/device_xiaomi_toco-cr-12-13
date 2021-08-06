11424f9f7b8405bdb05681dcc6b1ff6317ff0e9e
AndroidHQ254 <d.murimi14@gmail.com>
2021-08-07 00:32:21 +0300

    toco: Set built type to Fork LineageOS

--- lineage_toco.mk
+++ lineage_toco.mk
@@ -3,6 +3,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+# Flos Build Type
+TARGET_FLOS := true
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
