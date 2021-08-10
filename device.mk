3d603f204824e70235934a7f1108d8dcee95cb07
markkey014 <>
2021-08-10 20:37:45 +0300

    toco: Build FOD animation resources
    * Fixes SystemUI force close when pressing FOD on the lockscreen

--- device.mk
+++ device.mk
@@ -42,6 +42,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
+# FOD
+PRODUCT_PACKAGES += \
+    FodAnimationResources
+
 # Fstab
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
