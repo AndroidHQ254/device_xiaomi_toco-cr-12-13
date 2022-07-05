606eaf36baa59b216ce57337d89b9aa6cbb129ca
AndroidHQ254 <d.murimi14@gmail.com>
2022-07-06 02:54:46 +0300

    toco: Rename ANXCamera to MiuiCamera

--- device.mk
+++ device.mk
@@ -16,8 +16,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 # Inherit from sm6150-common
 $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
 
-# ANXCamera
-$(call inherit-product, vendor/ANXCamera/anx-vendor.mk)
+# MiuiCamera
+$(call inherit-product, vendor/MiuiCamera/config.mk)
 
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 29
