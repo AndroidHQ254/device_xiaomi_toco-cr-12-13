AndroidHQ254/android_device_xiaomi_toco-12-13

toco: Include firmware by default
Co-Authored-By: CHRISL7 <70589286+CHRISL7@users.noreply.github.com>

device.mk
@@ -19,6 +19,9 @@ $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
 # ANXCamera
 $(call inherit-product, vendor/ANXCamera/anx-vendor.mk)
 
+# Firmware Toco
+$(call inherit-product, vendor/xiaomi-firmware/toco/Android.mk)
+
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 29
 
@@ -137,7 +140,8 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    vendor/xiaomi-firmware
 
 # Storage health HAL
 PRODUCT_PACKAGES += \

AndroidHQ254/device_xiaomi_toco-cr-12-13

device.mk
@@ -26,6 +26,9 @@ $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
 # ANXCamera
 $(call inherit-product, vendor/ANXCamera/anx-vendor.mk)
 
+# Firmware Toco
+$(call inherit-product, vendor/xiaomi-firmware/toco/firmware.mk)
+
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 29
 
@@ -144,7 +147,8 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    vendor/xiaomi-firmware
 
 # VNDK
 PRODUCT_PACKAGES += \