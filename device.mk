bf21d4554b9c7ecc95789b2441e067c08d3f4e77
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-07-19 20:23:30 +0300

    toco: Build android.hidl.base@1.0

--- device.mk
+++ device.mk
@@ -60,6 +60,11 @@ PRODUCT_COPY_FILES += \
 # Get Hotword stuff
 $(call inherit-product-if-exists, vendor/xiaomi/google/google.mk)
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor
+
 # Hotword Enrollment
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hotword/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml \
